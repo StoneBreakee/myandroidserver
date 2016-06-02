69f07c5d8e66eca4daa88cd64c3c6336ce21b225
StoneBreakee <lvyanjie0616@sina.com>
2016-06-02 14:56:17 +0800

    add test case

---
+++ src/main/java/com/jenkins/hello/HelloJenkins.java
@@ -0,0 +1,8 @@
+package main.java.com.jenkins.hello;
+
+public class HelloJenkins
+{
+    public String sayHello(){
+        return "Hello,Jenkins";
+    }
+}
---
+++ src/test/java/com/jenkins/hello/HelloJenkinsTest.java
@@ -0,0 +1,19 @@
+package test.java.com.jenkins.hello;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import main.java.com.jenkins.hello.HelloJenkins;
+
+public class HelloJenkinsTest
+{
+
+    @Test
+    public void testSayHello()
+    {
+        HelloJenkins hello = new HelloJenkins();
+        assertEquals(hello.sayHello() , "Hello,Jenkins");
+    }
+
+}
